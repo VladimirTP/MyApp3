0efcbc732d003e35883d5ec9570ffb6f1bb6b24c
TamashovVladimir <v.tamashov@gmail.com>
2022-11-19 19:48:19 +0300

    taskArray1

--- app/src/main/java/com/example/myapp3/HomeworkArrays.java
+++ app/src/main/java/com/example/myapp3/HomeworkArrays.java
@@ -3,13 +3,12 @@ package com.example.myapp3;
 import java.util.Arrays;
 
 public class HomeworkArrays {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         taskArray1();
     }
-
     public static void taskArray1() {
-        String [] names = {"Alex", "Egor", "Maksim", "Ilya"};
-        Arrays.sort (names);
-        System.out.println(Arrays.toString(names));
+            String [] names = {"Alex", "Egor", "Maksim", "Ilya"};
+            Arrays.sort (names);
+            System.out.println(Arrays.toString(names));
     }
 }
